221397447f6cc20ac5a22a34aec2dc9488575068
guandjoy <guandjoy@gmail.com>
2019-12-29 23:56:19 +0200

    Set up tailwindcss

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      Button
+    </button>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
